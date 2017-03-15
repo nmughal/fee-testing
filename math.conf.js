eb4eddecc6b11537bc8a0bc5884dc7a7c1916d04
Nadia Mughal <nmughal14@gmail.com>
2017-03-15 16:20:56 -0400

    setted up the Karma test runner configuration file and added the package.json file

---
+++ math.conf.js
@@ -0,0 +1,11 @@
+module.exports = function karmConfig(config){
+  config.set({
+    frameworks: [ 'mocha', 'chai' ],
+    browsers: [ 'Chrome' ],
+    singleRun: true,
+    files: [
+      'src/**/*.js',
+      'test/specs/**/*.js'
+    ]
+  });
+};
